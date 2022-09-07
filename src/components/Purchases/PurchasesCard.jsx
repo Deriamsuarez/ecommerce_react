e2e5b9d2d422a792758511e6ddc5f8851c1d0b8f
Deriamsuarez <45581669+Deriamsuarez@users.noreply.github.com>
2022-09-07 17:57:34 -0400

    fixing somethings in css

--- src/components/Purchases/PurchasesCard.jsx
+++ src/components/Purchases/PurchasesCard.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const PurchasesCard = ({ purchase }) => {
+const PurchasesCard = ({ purchase, setId }) => {
 
     const navigate = useNavigate()
 
     const id = purchase.cartId
-
-    console.log(purchase.cartId)
     return (<>
 
-        <div onClick={() => navigate(`/purchase/${id}`)} className='purchaseItem'>
+{/* navigate(`/purchase/${id}`) */}
+        <div onClick={() => setId(purchase.cart.id)} className='purchaseItem'>
             <span>Invoice ID: <strong>{purchase.cart.id}</strong></span>
         </div>
     </>
